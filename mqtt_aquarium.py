b530e728c2dd5774abc5e7ed0b7edfcc1b8f9422
Alexander Leipnitz <leipnitz.alex@gmail.com>
2024-12-27 15:27:13 +0100

    Move thermometer functionality to separate file

--- mqtt_aquarium.py
+++ mqtt_aquarium.py
@@ -2,8 +2,8 @@ import paho.mqtt.client as mqtt
 import subprocess
 import sys
 import os
-import glob
 import time
+from thermometer import read_temp
 
 
 def get_environment_variable(name):
@@ -24,42 +24,9 @@ MQTT_TOPIC_WEBCAM = get_environment_variable("MQTT_TOPIC_WEBCAM")
 RECEIVER_IP = get_environment_variable("WEBCAM_RECEIVER_IP")
 RECEIVER_PORT = int(get_environment_variable("WEBCAM_RECEIVER_PORT"))
 
-# Setting up the thermometer:
-# ----------------------
-# These tow lines mount the device:
-os.system("modprobe w1-gpio")
-os.system("modprobe w1-therm")
-
-base_dir = "/sys/bus/w1/devices/"
-# Get all the filenames begin with 28 in the path base_dir.
-device_folder = glob.glob(base_dir + "28*")[0]
-device_file = device_folder + "/w1_slave"
-
 # Variables for ongoing recording and pipeline processes
 process_libcamera = None
 process_ffmpeg = None
-
-
-def read_temp_raw():
-    f = open(device_file, "r")
-    lines = f.readlines()
-    f.close()
-    return lines
-
-
-def read_temp():
-    lines = read_temp_raw()
-    # Analyze if the last 3 characters are 'YES'.
-    while lines[0].strip()[-3:] != "YES":
-        time.sleep(0.2)
-        lines = read_temp_raw()
-    # Find the index of 't=' in a string.
-    equals_pos = lines[1].find("t=")
-    if equals_pos != -1:
-        # Read the temperature .
-        temp_string = lines[1][equals_pos + 2 :]
-        temp_c = float(temp_string) / 1000.0
-        return temp_c
 
 
 # Callback for received MQTT messages
